e8ccc12098861feb202857d2239a43b62b828e5d
LohasOT <oliverhoang93@gmail.com>
2021-12-14 23:04:34 -0800

    added questions

---
+++ index.js
@@ -0,0 +1,8 @@
+let questions = [
+{
+  type: 'list',
+  message: "What would you like to do?",
+  choices: ['View All Employee', 'View All Employee By Department', 'View All Employee By Manager', 'Add Employee', 'Remove Emplyee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles', ''],
+  name: 'todo'
+}
+]

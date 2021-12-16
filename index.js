637e5c8136bd8c429026657ea5d79a3f9384c6c7
LohasOT <oliverhoang93@gmail.com>
2021-12-16 00:23:54 -0800

    remove typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const start = () => {
     {
   type: 'list',
   message: "What would you like to do?",
-  choices: ['View All Employee', 'View All Employee By Department', 'View All Employee By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles', ''],
+  choices: ['View All Employee', 'View All Employee By Department', 'View All Employee By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles'],
   name: 'todo'
     }
   ])
